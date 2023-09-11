ecdde1b17b020273712e61aadd5e7d70a88c2f50
haoli01 <144328588+haoli01@users.noreply.github.com>
2023-09-11 17:19:10 +0800

    hom.py
    计算纯合

--- hom.py
+++ hom.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 # -*- encoding: utf-8 -*-
 import argparse
- 
+import gzip
 parser = argparse.ArgumentParser(description='渐渗系纯合位点')
 parser.add_argument("input_vcf", type=str, help="need move local vcf file")
 args = parser.parse_args()
  
 input_vcf = args.input_vcf
-fh = open(input_vcf)
+fh = gzip.open(input_vcf,'rt')
 for x in fh:
     if x.startswith('#'):
         print(x.strip())
